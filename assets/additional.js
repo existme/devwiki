e3649fcd9cdf30df875cba3fe7354ae5ba32eecd
Ola Montán <olamo@axis.com>
2016-12-23 20:33:17 +0100

    Adding an expandable left menu

--- assets/additional.js
+++ assets/additional.js
@@ -1,1 +1,22 @@
 $('h1').slice(2).before("<br><hr>");
+
+$("#jstree_div").jstree({
+    "conditionalselect" : function (node) {
+        return false;
+    },
+    "plugins" : ['conditionalselect'],
+//        "core" : { // core options go here
+//            "themes" : { "icons": false },
+//        },
+    "state" : { "key" : "state_wiki" },
+    "plugins" : ["state"],
+});
+
+$(document).on('click', '.jstree-anchor', function(e) {
+    var page = $(this).parent().attr("page");
+    if (page != null) {
+        window.location.href = page;
+    }
+});
+
+$(function () { $('#jstree_div').jstree(); });
