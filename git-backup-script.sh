d6f640da7d0d8ff135704e38ee7098f02738d5a3
existme <existme@gmail.com>
2017-08-13 11:14:35 +0200

    updated submodule and backup script

--- git-backup-script.sh
+++ git-backup-script.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+cd /media/pi/My\ Book/git/wiki/wiki/en
+#git checkout master
+rm /media/pi/My\ Book/git/wiki/.git/modules/wiki/en/index.lock
+/usr/bin/env python gen_sidebar
+git add .
+git commit -a -m "Auto updating wiki draft changes"
+git pull
+git push
+
+cd /media/pi/My\ Book/git/wiki
+git submodule update
 cd /media/pi/My\ Book/git/wiki
 
 #git checkout draft
@@ -8,13 +19,3 @@ git add .
 git commit -a -m "Auto updating bananadance changes"
 git push
 
-cd wiki/en
-#git checkout master
-rm /media/pi/My\ Book/git/wiki/.git/modules/wiki/en/index.lock
-/usr/bin/env python gen_sidebar
-git add .
-git commit -a -m "Auto updating wiki draft changes"
-git pull
-git push
-cd /media/pi/My\ Book/git/wiki
-git submodule update
